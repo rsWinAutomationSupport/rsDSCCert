75d0cac25ce297a92158d02f9218a4f08fb738a0
Mark Key <mark.key@rackspace.com>
2015-04-28 12:56:11 -0500

    init

---
+++ DSCResources/RS_rsPullCert/RS_rsPullCert.psm1
@@ -0,0 +1,37 @@
+﻿Function Get-TargetResource {
+  param (
+    [parameter(Mandatory = $true)][string]$Name
+  )
+  return @{
+    'Name' = $Name
+  }
+}
+
+Function Test-TargetResource {
+  param (
+    [parameter(Mandatory = $true)][string]$Name
+  )
+  if((Get-ChildItem -Path Cert:\LocalMachine\Root\ | Where-Object -FilterScript {$_.Subject -eq $('CN=', $env:COMPUTERNAME -join '')}).Thumbprint -eq (Get-ChildItem -Path Cert:\LocalMachine\My\ | Where-Object -FilterScript {$_.Subject -eq $('CN=', $env:COMPUTERNAME -join '')}).Thumbprint) {
+    return $true
+  }
+  else {
+    return $false
+  }
+
+}
+
+Function Set-TargetResource {
+  param (
+    [parameter(Mandatory = $true)][string]$Name
+  )
+  $yesterday = (Get-Date).AddDays(-1) | Get-Date -Format MM/dd/yyyy
+  Get-ChildItem -Path Cert:\LocalMachine\My\ | Where-Object -FilterScript {$_.Subject -eq $('CN=', $env:COMPUTERNAME -join '')} | Remove-Item
+  & makecert.exe -b $yesterday -r -pe -n $('CN=', $env:COMPUTERNAME -join ''), -ss my, -sr localmachine, -len 2048
+  Get-ChildItem -Path Cert:\LocalMachine\Root\ | Where-Object -FilterScript {$_.Subject -eq $('CN=', $env:COMPUTERNAME -join '')} | Remove-Item
+  $store = Get-Item Cert:\LocalMachine\Root
+  $store.Open([System.Security.Cryptography.X509Certificates.OpenFlags]'ReadWrite')
+  $store.Add( $(New-Object System.Security.Cryptography.X509Certificates.X509Certificate -ArgumentList @(,(Get-ChildItem Cert:\LocalMachine\My | ? Subject -eq "CN=$env:COMPUTERNAME").RawData)) )
+  $store.Close()
+}
+
+Export-ModuleMember -Function *-TargetResource
---
+++ rsDSCCert.psd1
@@ -0,0 +1,31 @@
+@{
+# Version number of this module.
+ModuleVersion = '1.0'
+
+# ID used to uniquely identify this module
+GUID = '76106d12-22a8-4935-913f-ff442fa49698'
+
+# Author of this module
+Author = 'Rackspace'
+
+# Company or vendor of this module
+CompanyName = 'Rackspace'
+
+# Copyright statement for this module
+Copyright = '(c) 2014 Rackspace. All rights reserved.'
+
+# Description of the functionality provided by this module
+Description = 'Module with DSC Resources for rsDSCCert area'
+
+# Minimum version of the Windows PowerShell engine required by this module
+PowerShellVersion = '5.0'
+
+# Minimum version of the common language runtime (CLR) required by this module
+CLRVersion = '4.0'
+
+# Functions to export from this module
+FunctionsToExport = '*'
+
+# Cmdlets to export from this module
+CmdletsToExport = '*'
+}
