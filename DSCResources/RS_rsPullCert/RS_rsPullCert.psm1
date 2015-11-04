ae9356c8a6f50ef2f9e1cc3889568a97c2e4170d
Alexei Andreyev <alexei.andreyev@rackspace.co.uk>
2015-11-04 15:04:03 +0000

    Fix code to copy pull cert to root store Add some logic to better-handle removals of redundant certs

--- DSCResources/RS_rsPullCert/RS_rsPullCert.psm1
+++ DSCResources/RS_rsPullCert/RS_rsPullCert.psm1
@@ -34,21 +34,51 @@ Function Set-TargetResource {
 
   $d = Get-Content $(Join-Path ([Environment]::GetEnvironmentVariable('defaultPath','Machine')) 'secrets.json') -Raw | ConvertFrom-Json
   
-  Get-ChildItem -Path Cert:\LocalMachine\My\ | Where-Object -FilterScript {$_.Subject -eq $('CN=', $d.PullServerAddress -join '')} | Remove-Item
-
-  $EndDate = (Get-Date).AddYears(25) | Get-Date -Format MM/dd/yyyy
-  New-SelfSignedCertificateEx -Subject $('CN=', $d.PullServerAddress -join '') `
-                              -NotAfter $EndDate `
-                              -StoreLocation LocalMachine `
-                              -StoreName My `
-                              -Exportable `
-                              -KeyLength 2048
-
-  Get-ChildItem -Path Cert:\LocalMachine\Root\ | Where-Object -FilterScript {$_.Subject -eq $('CN=', $d.PullServerAddress -join '')} | Remove-Item
-  $store = Get-Item Cert:\LocalMachine\Root
-  $store.Open([System.Security.Cryptography.X509Certificates.OpenFlags]'ReadWrite')
-  $store.Add( $(New-Object System.Security.Cryptography.X509Certificates.X509Certificate -ArgumentList @(,(Get-ChildItem Cert:\LocalMachine\My | Where-Object Subject -eq "CN=$d.PullServerAddress").RawData)) )
-  $store.Close()
+  # Check if a self-signed Pull server certificate already exists - create it if it is not present
+  $PullCertThumbprint = (Get-ChildItem -Path Cert:\LocalMachine\My\ | Where-Object -FilterScript {$_.Subject -eq $('CN=', $d.PullServerAddress -join '')}).Thumbprint
+  if ($PullCertThumbprint -eq $null)
+  {
+    $EndDate = (Get-Date).AddYears(25) | Get-Date -Format MM/dd/yyyy
+    New-SelfSignedCertificateEx -Subject $('CN=', $d.PullServerAddress -join '') `
+                                -NotAfter $EndDate `
+                                -StoreLocation LocalMachine `
+                                -StoreName My `
+                                -Exportable `
+                                -KeyLength 2048
+    $PullCertThumbprint = (Get-ChildItem -Path Cert:\LocalMachine\My\ | Where-Object -FilterScript {$_.Subject -eq $('CN=', $d.PullServerAddress -join '')}).Thumbprint
+  }
+  
+  # Replace pull certificate in root store if it does not match one in system personal store
+  $RootPullCertThumbprint = (Get-ChildItem -Path Cert:\LocalMachine\Root\ | Where-Object -FilterScript {$_.Subject -eq $('CN=', $d.PullServerAddress -join '')}).Thumbprint
+  
+  if ( -not ($PullCertThumbprint -eq $RootPullCertThumbprint))
+  {
+    if ($RootPullCertThumbprint -ne $null)
+    {
+      Get-ChildItem -Path Cert:\LocalMachine\Root\ | Where-Object -FilterScript {$_.thumbprint -eq $RootPullCertThumbprint} | Remove-Item
+    }
+  
+    # Copy the Pull server certificate to the root store
+    $SourceStoreScope = 'LocalMachine'
+    $SourceStorename = 'My'
+    
+    $SourceStore = New-Object  -TypeName System.Security.Cryptography.X509Certificates.X509Store  -ArgumentList $SourceStorename, $SourceStoreScope
+    $SourceStore.Open([System.Security.Cryptography.X509Certificates.OpenFlags]::ReadOnly)
+    $PullCertObj = $SourceStore.Certificates | 
+                    Where-Object -FilterScript {
+                        $_.Thumbprint -eq $PullCertThumbprint
+                    }
+    
+    $DestStoreScope = 'LocalMachine'
+    $DestStoreName = 'root'
+    
+    $DestStore = New-Object  -TypeName System.Security.Cryptography.X509Certificates.X509Store  -ArgumentList $DestStoreName, $DestStoreScope
+    $DestStore.Open([System.Security.Cryptography.X509Certificates.OpenFlags]::ReadWrite)
+    $DestStore.Add($PullCertObj)
+    
+    $SourceStore.Close()
+    $DestStore.Close()
+  }
 }
 
 Export-ModuleMember -Function *-TargetResource
