fe765421e93bb2b2d0ea8b36345577332c1486d7
Alexei Andreyev <alexei.andreyev@rackspace.co.uk>
2015-11-04 13:47:54 +0000

    Replace makecert.exe with  New-SelfSignedCertificateEx Related to issue #3

--- DSCResources/RS_rsPullCert/RS_rsPullCert.psm1
+++ DSCResources/RS_rsPullCert/RS_rsPullCert.psm1
@@ -25,14 +25,29 @@ Function Set-TargetResource {
   param (
     [parameter(Mandatory = $true)][string]$Name
   )
+  # Import extra functions for use in the resource
+  Get-Item (Join-Path -Path $PSScriptRoot -ChildPath 'helper_scripts\*.ps1') | 
+    ForEach-Object {
+        Write-Verbose ("Importing " -f $_.FullName)
+        . $_.FullName
+    }
+
   $d = Get-Content $(Join-Path ([Environment]::GetEnvironmentVariable('defaultPath','Machine')) 'secrets.json') -Raw | ConvertFrom-Json
-  $yesterday = (Get-Date).AddDays(-1) | Get-Date -Format MM/dd/yyyy
+  
   Get-ChildItem -Path Cert:\LocalMachine\My\ | Where-Object -FilterScript {$_.Subject -eq $('CN=', $d.PullServerAddress -join '')} | Remove-Item
-  & makecert.exe -b $yesterday -r -pe -n $('CN=', $d.PullServerAddress -join ''), -ss my, -sr localmachine, -len 2048
+
+  $EndDate = (Get-Date).AddYears(25) | Get-Date -Format MM/dd/yyyy
+  New-SelfSignedCertificateEx -Subject $('CN=', $d.PullServerAddress -join '') `
+                              -NotAfter $EndDate `
+                              -StoreLocation LocalMachine `
+                              -StoreName My `
+                              -Exportable `
+                              -KeyLength 2048
+
   Get-ChildItem -Path Cert:\LocalMachine\Root\ | Where-Object -FilterScript {$_.Subject -eq $('CN=', $d.PullServerAddress -join '')} | Remove-Item
   $store = Get-Item Cert:\LocalMachine\Root
   $store.Open([System.Security.Cryptography.X509Certificates.OpenFlags]'ReadWrite')
-  $store.Add( $(New-Object System.Security.Cryptography.X509Certificates.X509Certificate -ArgumentList @(,(Get-ChildItem Cert:\LocalMachine\My | ? Subject -eq "CN=$d.PullServerAddress").RawData)) )
+  $store.Add( $(New-Object System.Security.Cryptography.X509Certificates.X509Certificate -ArgumentList @(,(Get-ChildItem Cert:\LocalMachine\My | Where-Object Subject -eq "CN=$d.PullServerAddress").RawData)) )
   $store.Close()
 }
 
